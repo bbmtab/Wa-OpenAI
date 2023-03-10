4f3ced8a1644e485b46dffd9d7ed4125d82258c1
bbmtab <121282581+bbmtab@users.noreply.github.com>
2023-03-10 09:25:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const sessionName = "yusril";
+const sessionName = process.env.SESSION || "yusril";
 const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const owner = [process.env.NO || "6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
