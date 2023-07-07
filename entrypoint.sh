66739a235db053ecab9f52edd19c5f70772c6fda
bbmtab <121282581+bbmtab@users.noreply.github.com>
2023-07-07 20:09:26 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+!/bin/sh
 # Replace "key" with the value of KEY in the key.json file
-#sed -i "s/\"keyopenai\": \"key\"/\"keyopenai\": \"${KEY}\"/" /app/key.json
+sed -i "s/\"keyopenai\": \"key\"/\"keyopenai\": \"${KEY}\"/" /app/key.json
 
 # Start the Node.js application
-npm run dev
+node index.js
